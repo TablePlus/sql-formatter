7740afee2af5bd1108619d1d451b706399b218a6
Huy Pham <duchuykun@gmail.com>
2020-01-10 23:57:40 -0400

    Fixed bug build.

--- SQLFormatter.tableplusplugin/webpack.config.js
+++ SQLFormatter.tableplusplugin/webpack.config.js
@@ -4,23 +4,10 @@ const path = require('path');
 const config = {
     entry: './main.js',
     output: {
-        filename: 'format.js'
+    	path: path.resolve(__dirname, './'),
+    	filename: './format.js'
     },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015']
-            }
-        }]
-    },
-    stats: {
-        colors: true
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin()
-    ]
+    mode: 'production'
 };
 
 module.exports = config;
